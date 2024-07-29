00c52a92c3e3559028b6553908b33602ed1198ec
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-07-29 22:48:24 +0300

    Get form values from formdata

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const myLibrary = [];
 
 function Book(name, author, pages, read) {
     this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
-    addBookToLibrary(this);
+    // this.author = author;
+    // this.pages = pages;
+    // this.read = read;
 }
-  
+
+const bookList = document.querySelector(".booklist");
+const form = document.querySelector(".form");
+
+form.addEventListener(("submit"), () => {
+
+    const formData = new FormData(form);
+    for (const [key, value] of formData) {
+        console.log(`${key}: ${value}`);
+    }
+})
+
 function addBookToLibrary(Book) {
+    bookList.textContent += Book.name;
     myLibrary.push(Book);
 }
