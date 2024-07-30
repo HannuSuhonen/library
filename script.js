af550cec63a7730b89c9ba9965c694532184cf83
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-07-30 11:18:09 +0300

    Create new book objects from form

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const myLibrary = [];
+const KEYBOOKNAME = "bookName";
+const KEYAUTHORNAME = "authorName";
+const KEYPAGESCOUNT = "pagesCount";
+const KEYREAD = "read";
 
 function Book(name, author, pages, read) {
     this.name = name;
-    // this.author = author;
-    // this.pages = pages;
-    // this.read = read;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+const form = document.querySelector(".form");
+form.addEventListener(("submit"), () => {
+    const formData = new FormData(form);
+    addBookToLibrary(formData);
+})
+
+function addBookToLibrary(formData) {
+    const newBook = new Book(
+    formData.get(KEYBOOKNAME),
+    formData.get(KEYAUTHORNAME),
+    formData.get(KEYPAGESCOUNT),
+    formData.get(KEYREAD));
+
+    myLibrary.push(newBook);
+    updateBookList(newBook);
 }
 
 const bookList = document.querySelector(".booklist");
-const form = document.querySelector(".form");
-
-form.addEventListener(("submit"), () => {
-
-    const formData = new FormData(form);
-    for (const [key, value] of formData) {
-        console.log(`${key}: ${value}`);
-    }
-})
-
-function addBookToLibrary(Book) {
-    bookList.textContent += Book.name;
-    myLibrary.push(Book);
+function updateBookList(book){
+    bookList.textContent += book;
 }
